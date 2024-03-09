1c773c1bd5f9ae53b9a30e9dc31d402486d08096
han <rudgksdl94@gmail.com>
2024-03-10 00:29:17 +0900

    고객 레퍼지토리

---
+++ src/main/java/com/example/cms_project/user/client/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.cms_project.user.client.repository;
+
+import com.example.cms_project.user.client.model.Customer;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+  Optional<Customer> findByEmail(String email);
+}
