563f1f2883b0e2c42631a4b763bbf3ed4bdd8390
han <rudgksdl94@gmail.com>
2024-03-10 01:32:40 +0900

    로그인 api - 컨트롤러, 폼

---
+++ src/main/java/com/example/cms_project/user/client/controller/SignInController.java
@@ -0,0 +1,22 @@
+package com.example.cms_project.user.client.controller;
+
+import com.example.cms_project.user.client.application.SignInApplication;
+import com.example.cms_project.user.client.domain.SignInForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signIn")
+@RequiredArgsConstructor
+public class SignInController {
+
+  private final SignInApplication signInApplication;
+  @PostMapping("/customer")
+  public ResponseEntity<String> signInCustomer(@RequestBody SignInForm form) {
+    return ResponseEntity.ok(signInApplication.customerLoginToken(form));
+  }
+}
